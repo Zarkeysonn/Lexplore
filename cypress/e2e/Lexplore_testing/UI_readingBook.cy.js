5dca3f8bfb98462403a21ddc9e5bfb36a22c40b7
Zarko Djurdjev <zarko.djurdjev@automaticity.rs>
2023-12-06 15:21:10 +0100

    Added restoring page read in before

--- cypress/e2e/Lexplore_testing/UI_readingBook.cy.js
+++ cypress/e2e/Lexplore_testing/UI_readingBook.cy.js
@@ -3,25 +3,39 @@ import login from "../../api/auth";
 import loginData from "../../fixtures/login.json";
 import { navigation } from "../../pageObjectModel/navigation";
 import { library } from "../../pageObjectModel/libraryReading";
-import bookApi from "../../api/books";
+import bookApi from "../../api/books.js";
 
-/*
-Dograbi api details od knjige i iz responsa procitaj numberOfPages, 
-*/
 describe("Reading book", () => {
-  before(() => {
+  beforeEach(() => {
+    library.unreadBook({
+      books: [data.gulhojBook, data.epubBook, data.tbaBook],
+    });
+    cy.viewport(1280, 720);
     login.login({});
-    cy.url().should("eq", loginData.homePageUrl);
+
+    //cy.url().should("eq", loginData.homePageUrl);
     navigation.navigateTo(data.navigateTo.library);
+    library.restartReadPages({ bookId: data.whalesBookID });
   });
-  it("Read book to the end", () => {
-    //library.getBookID(2); //response body sa detailjima knjige
-    //for (let i; i < 3; i++) {
-    library.readingProgress({ option: 2 }); //46 epub, 2 je gulhoj
-    library.showProgressBtn.click();
-    library.bookProgressPercentage.should("have.text", "100%");
+  context("Read book to the end", () => {
+    it("Read gulhoj book to the end", () => {
+      library.readingProgress({ bookName: data.gulhojBook, option: 1 });
+      //bookApi.restartReadPages({ bookId: data.whalesBookID });
+    });
+
+    it("Read EPUB book", () => {
+      library.readingProgress({ bookName: data.epubBook, option: 1 });
+    });
+
+    it("Read tba book", () => {
+      library.readingProgress({ bookName: data.tbaBook, option: 1 });
+    });
   });
-  after(() => {
-    //obrisi tj. vrati na pocetno stanje
+
+  afterEach(() => {
+    //
+    // bookApi.restartReadPages({
+    //   bookId:
+    // })
   });
 });
